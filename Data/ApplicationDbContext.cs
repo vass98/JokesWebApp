b5838cc971c41b4523d6102a8fe5419b137a627d
Vasu Kaushik <kaushikvasu98@gmail.com>
2023-06-08 18:54:54 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using jokeswebapp.Models;
+
+namespace jokeswebapp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<jokeswebapp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
